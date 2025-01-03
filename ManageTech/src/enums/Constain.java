29b7206c7196b8b5ce17499a4a0cb90c31783a70
HieuNguyen06nd <nguyenhieu06.nd@gmail.com>
2025-01-04 04:10:12 +0700

    update data

--- ManageTech/src/enums/Constain.java
+++ ManageTech/src/enums/Constain.java
@@ -4,13 +4,13 @@ public class Constain {
     public static final int MON_WED_FRI = 1;
     public static final int TUE_THU_SAT = 2;
 
-    public static final String TEACHERS_FILE_PATH = "teachers.json";
-    public static final String STUDENTS_FILE_PATH = "students.json";
-    public static final String USERS_FILE_PATH = "users.json"; // File chứa Admin và Customer
-    public static final String COURSES_FILE_PATH = "courses.json";
-    public static final String CLASSES_FILE_PATH = "classes.json";
-    public static final String LESSONS_FILE_PATH = "lessons.json"; // Thêm đường dẫn cho Lesson
-    public static final String ENROLLMENTS_FILE_PATH = "enrollments.json";
-    public static final String SCORES_FILE_PATH = "scores.json";
-    public static final String BLOGS_FILE_PATH = "blogs.json";
+    public static final String TEACHERS_FILE_PATH = "managedata/teachers.json";
+    public static final String STUDENTS_FILE_PATH = "managedata/students.json";
+    public static final String USERS_FILE_PATH = "managedata/users.json"; // File chứa Admin và Customer
+    public static final String COURSES_FILE_PATH = "managedata/courses.json";
+    public static final String CLASSES_FILE_PATH = "managedata/classes.json";
+    public static final String LESSONS_FILE_PATH = "managedata/lessons.json"; // Thêm đường dẫn cho Lesson
+    public static final String ENROLLMENTS_FILE_PATH = "managedata/enrollments.json";
+    public static final String SCORES_FILE_PATH = "managedata/scores.json";
+    public static final String BLOGS_FILE_PATH = "managedata/blogs.json";
 }
